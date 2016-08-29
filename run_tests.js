37318e374af11fbb02f96e731bc4688cba56c7f8
H_ELI_X <TylerEli617@gmail.com>
2016-08-29 15:21:54 -0400

    Use merp_print_test.js to print the test results

--- run_tests.js
+++ run_tests.js
@@ -1,73 +1,19 @@
 var NodeTest = require("merp_node_test.js");
+var PrintTest = require("merp_print_test.js");
 
 var path = require("path");
 
 var print = console.log;
 
-findAndRunTestSuites = NodeTest.findAndRunTestSuites;
-
-function countResults(results)
-{
-    var count = 0;
-
-    for (var suiteIndex = 0; suiteIndex < results.testSuiteResults.length; suiteIndex++)
-    {
-        count += results.testSuiteResults[suiteIndex].testCaseResults.length;
-    }
-
-    return count;
-}
-
-function countPassedResults(results)
-{
-    var count = 0;
-
-    for (var suiteIndex = 0; suiteIndex < results.passedTestSuiteResults.length; suiteIndex++)
-    {
-        count += results.passedTestSuiteResults[suiteIndex].passedTestCaseResults.length;
-    }
-
-    return count;
-}
-
-function countFailedResults(results)
-{
-    var count = 0;
-
-    for (var suiteIndex = 0; suiteIndex < results.failedTestSuiteResults.length; suiteIndex++)
-    {
-        count += results.failedTestSuiteResults[suiteIndex].failedTestCaseResults.length;
-    }
-
-    return count;
-}
-
-function printFailed(results)
-{
-    for (var suiteIndex = 0; suiteIndex < results.failedTestSuiteResults.length; suiteIndex++)
-    {
-        var suite = results.failedTestSuiteResults[suiteIndex];
-
-        for (var caseIndex = 0; caseIndex < suite.failedTestCaseResults.length; caseIndex++)
-        {
-            print(suite.failedTestCaseResults[caseIndex]);
-        }
-    }
-}
+var findAndRunTestSuites = NodeTest.findAndRunTestSuites;
+var countFailedResults = PrintTest.countFailedResults;
+var printResults = PrintTest.getPrintResults(print);
 
 function testsDone(results)
 {
-    var totalTests = countResults(results);
-    var passedTests = countPassedResults(results);
-    var failedTests = countFailedResults(results);
+    printResults(results);
 
-    print("Ran    : " + totalTests);
-    print("Passed : " + passedTests);
-    print("Failed : " + failedTests);
-
-    printFailed(results);
-
-    if (module.simpleTestRan && (failedTests === 0))
+    if (module.simpleTestRan && (countFailedResults(results) === 0))
     {
         process.exit(0);
     }
